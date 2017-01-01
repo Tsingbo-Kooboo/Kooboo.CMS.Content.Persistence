aa7a206765441f3d54fe3ead38fb30f2075876ef
Tsingbo-Kooboo <qingbo@kooboo.com>
2017-01-01 12:49:01 +0800

    release

--- src/CMSAssemblyInfoGlobal.cs
+++ src/CMSAssemblyInfoGlobal.cs
@@ -22,5 +22,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.5.0.1230")]
-[assembly: AssemblyFileVersion("4.5.0.1230")]
+[assembly: AssemblyVersion("4.5.1.0101")]
+[assembly: AssemblyFileVersion("4.5.1.0101")]
